431918ccebc1c342cefe28b1eacd4373c48292d3
KETHAVATH RAKESH NAIK <114587685+KRakeshnaik@users.noreply.github.com>
2024-08-18 16:41:19 +0530

    Add files via upload

---
+++ src/main/java/nanotech/App.java
@@ -0,0 +1,13 @@
+package nanotech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
